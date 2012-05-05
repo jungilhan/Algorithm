c6552977b0dd97fa796b591866f86f3912c4ad0c
Jungil Han <jungil.han@gmail.com>
2012-05-05 17:31:22 +0900

    이진 검색 파이썬 코드 추가

---
+++ Python/binarysearch.py
@@ -0,0 +1,18 @@
+def search(array, value):
+        return binarysearch(array, 0, len(array) - 1, value)
+
+def binarysearch(array, low, high, value):
+        if low > high:
+                return -1
+
+        mid = (low + high) / 2
+        if array[mid] == value:
+                return mid
+        elif array[mid] < value:
+                return binarysearch(array, mid + 1, high, value) 
+        elif array[mid] > value:
+                return binarysearch(array, low, mid - 1, value)
+ 
+if __name__ == '__main__':
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        print search(array, 90)
